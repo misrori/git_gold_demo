ead93049fde245fe9c14b50281a62f4c7df3a36e
Mihály Orsós <ormraat.pte@gmail.com>
2026-05-11 14:41:05 +0200

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -5,3 +5,13 @@ params = {"ids": "bitcoin", "vs_currencies": "usd"}
 valasz = requests.get(url, params=params)
 ar = valasz.json()["bitcoin"]["usd"]
 print(f"Bitcoin ára: ${ar:,.0f}")
+
+
+import requests
+
+url = "https://api.coingecko.com/api/v3/simple/price"
+params = {"ids": "bitcoin,ethereum", "vs_currencies": "usd"}
+valasz = requests.get(url, params=params)
+adat = valasz.json()
+print(f"Bitcoin:  ${adat['bitcoin']['usd']:,.0f}")
+print(f"Ethereum: ${adat['ethereum']['usd']:,.0f}")
